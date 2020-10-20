515bdf965d65e4842537b63c691c973c05189f80
Toby <toby@thedevlins.biz>
2020-10-20 10:21:10 +0100

    update search all

--- cmd/search.go
+++ cmd/search.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-var queryString string
+var phraseString string
 var all bool
 var limit int
 
@@ -18,26 +18,32 @@ var searchCmd = &cobra.Command{
 	Short:   "Search all your notes for a string",
 	Aliases: []string{"s"},
 	Run: func(cmd *cobra.Command, args []string) {
-		if queryString == "" {
+		var queryString string
+		if phraseString == "" && !all {
 			queryString = internal.TakeInput("Search query:", false)
+		} else {
+			queryString = phraseString
 		}
+
 		if queryString == "" && !all {
 			fmt.Println("No search param given. To retrieve all notes pass flag --all")
+			return
 		}
+
 		notes := internal.SearchNotes(queryString, limit)
 		if len(notes) > 0 {
 			for _, note := range notes {
 				fmt.Println(note.CreatedAt.Format(viper.GetString("date_format")), "-", note.Content)
 			}
 		} else {
-			fmt.Printf("No results for search \"%s\"", queryString)
+			fmt.Printf("No results for \"%s\"", queryString)
 		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(searchCmd)
-	searchCmd.Flags().StringVarP(&queryString, "phrase", "p", "", "Pass the search phrase directly to the command line rather than interactively")
+	searchCmd.Flags().StringVarP(&phraseString, "phrase", "p", "", "Pass the search phrase directly to the command line rather than interactively")
 	searchCmd.Flags().BoolVarP(&all, "all", "a", false, "Return all notes")
 	searchCmd.Flags().IntVarP(&limit, "limit", "l", 10, "The max number of notes to return")
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mattn/go-sqlite3 v1.14.4 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
+	github.com/pelletier/go-toml v1.2.0
 	github.com/spf13/cobra v1.1.0
 	github.com/spf13/viper v1.7.1
 	gorm.io/driver/sqlite v1.1.3
